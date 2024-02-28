2c2ab3bf2608ac9881389f4f316dbebae1480fa0
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-02-28 12:30:50 +0000

    HomePage  fully responsive done.

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector("#menu-icon");
+let navmenu = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-bars");
+  menu.classList.toggle("fa-xmark");
+  navmenu.classList.toggle("open");
+};
