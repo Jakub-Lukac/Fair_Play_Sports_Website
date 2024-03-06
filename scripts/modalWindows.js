5c24e459abbb5c0eff756cb631a0d00376c19fd7
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-03-06 10:07:13 +0000

    idk

--- scripts/modalWindows.js
+++ scripts/modalWindows.js
@@ -1,15 +1,29 @@
 $(document).ready(function () {
   // Handle click on playBtn
   $("div.playBtn").click(function (e) {
+    console.log(1);
     var videoUrl = $(this).data("video-url");
     console.log("Video URL:", videoUrl); // Debugging
     $("video.modalVid").attr("src", videoUrl);
 
     $("section#modal").css({ display: "block", opacity: 0 });
-    $("video.modalVid").css({ marginTop: "-50px" });
+    $("video.modalVid").css({ display: "block", marginTop: "-50px" });
+    $("div.players-stat-table").css({ display: "none" });
 
     $("section#modal").animate({ opacity: 1 }, 500);
     $("video.modalVid").animate({ marginTop: "0px" }, 500);
+  });
+
+  $(".full-stats-link").click(function (e) {
+    console.log(2);
+    e.preventDefault(); // Prevent default link behavior
+
+    $("section#modal").css({ display: "block", opacity: 0 });
+    $("div.players-stat-table").css({ display: "block", marginTop: "-50px" });
+    $("video.modalVid").css({ display: "none" });
+
+    $("section#modal").animate({ opacity: 1 }, 500);
+    $("div.players-stat-table").animate({ marginTop: "0px" }, 500);
   });
 
   // Close modal window when click on black background
@@ -24,5 +38,6 @@ $(document).ready(function () {
       }
     );
     $("video#modalVid").animate({ marginTop: "+50px" }, 500);
+    $("div.players-stat-table").animate({ marginTop: "+50px" }, 500);
   });
 });
