a5527eacfa791303f640c3709e261fc865646379
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-03-17 17:48:14 +0000

    Troubleshooting

--- scripts/scroll-reveal/homePage.js
+++ scripts/scroll-reveal/homePage.js
@@ -6,8 +6,35 @@ const sr = ScrollReveal({
 
 /* HERO SECTION */
 sr.reveal(".logoImage", { delay: 300, origin: "bottom" });
-sr.reveal(".left", { delay: 400, origin: "left" });
-sr.reveal(".right", { delay: 400, origin: "right" });
+
+function revealElements() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth >= 1050) {
+    sr.reveal(".left", { delay: 400, origin: "left" });
+    sr.reveal(".right", { delay: 400, origin: "right" });
+  } else {
+    // if we have hamburger menu, we do not want to have the loading animation
+    // as the elements wont display properly
+    const leftElement = document.querySelector(".left");
+    const rightElement = document.querySelector(".right");
+
+    if (leftElement) {
+      leftElement.classList.remove("load-hidden");
+    }
+
+    if (rightElement) {
+      rightElement.classList.remove("load-hidden");
+    }
+  }
+}
+
+// Call revealElements initially to handle the initial page load
+revealElements();
+
+// Call revealElements whenever the window is resized
+window.addEventListener("resize", revealElements);
+/*sr.reveal(".left", { delay: 400, origin: "left" });
+sr.reveal(".right", { delay: 400, origin: "right" });*/
 
 sr.reveal(".hero-content", { delay: 450, origin: "left" });
 sr.reveal(".social-media-btns", { delay: 600, origin: "bottom" });
@@ -23,4 +50,4 @@ sr.reveal(".application-card", { delay: 450, origin: "right" });
 sr.reveal(".sponsors-image", { delay: 500, origin: "left" });
 
 /* FOOTER */
-sr.reveal(".footer-text", {delay: 600, origin: "bottom"});
+sr.reveal(".footer-text", { delay: 600, origin: "bottom" });
--- scripts/scroll-reveal/sponsorship.js
+++ scripts/scroll-reveal/sponsorship.js
@@ -6,8 +6,34 @@ const sr = ScrollReveal({
 
 /* NAVBAR */
 sr.reveal(".logoImage", { delay: 300, origin: "bottom" });
-sr.reveal(".left", { delay: 400, origin: "left" });
-sr.reveal(".right", { delay: 400, origin: "right" });
+function revealElements() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth >= 1050) {
+    sr.reveal(".left", { delay: 400, origin: "left" });
+    sr.reveal(".right", { delay: 400, origin: "right" });
+  } else {
+    // if we have hamburger menu, we do not want to have the loading animation
+    // as the elements wont display properly
+    const leftElement = document.querySelector(".left");
+    const rightElement = document.querySelector(".right");
+
+    if (leftElement) {
+      leftElement.classList.remove("load-hidden");
+    }
+
+    if (rightElement) {
+      rightElement.classList.remove("load-hidden");
+    }
+  }
+}
+
+// Call revealElements initially to handle the initial page load
+revealElements();
+
+// Call revealElements whenever the window is resized
+window.addEventListener("resize", revealElements);
+/*sr.reveal(".left", { delay: 400, origin: "left" });
+sr.reveal(".right", { delay: 400, origin: "right" });*/
 sr.reveal(".social-media-btns", { delay: 600, origin: "bottom" });
 
 /* SUPPORT SECTION */
